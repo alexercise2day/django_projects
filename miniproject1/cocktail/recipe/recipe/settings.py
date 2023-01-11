0598dedf1c62109c08b3dd2af8b72fbf3f7064f4
alexlim <aml924@nyu.edu>
2023-01-11 14:08:56 -0500

    fixed error so can runserver. todo: fix templates

--- miniproject1/cocktail/recipe/recipe/settings.py
+++ miniproject1/cocktail/recipe/recipe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cocktailDRF",
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "cocktailDRF"
 ]
 
 ROOT_URLCONF = "recipe.urls"
