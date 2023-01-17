fe4154a46ead2e29dbaa253345012afd861936b5
alexlim <aml924@nyu.edu>
2023-01-17 15:54:38 -0500

    made new classes

--- miniproject1/cocktail/recipe/cocktailDRF/admin.py
+++ miniproject1/cocktail/recipe/cocktailDRF/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import cocktail
+from .models import cocktail, Vodka
 
 # Register your models here.
 admin.site.register(cocktail)
+admin.site.register(Vodka)
--- miniproject1/cocktail/recipe/cocktailDRF/models.py
+++ miniproject1/cocktail/recipe/cocktailDRF/models.py
@@ -6,3 +6,7 @@ class cocktail(models.Model):
     ingredients = models.TextField(max_length=500)
     instructions = models.TextField(max_length=500)
     # image = models.ImageField(upload_to='images/')
+
+class Vodka(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
