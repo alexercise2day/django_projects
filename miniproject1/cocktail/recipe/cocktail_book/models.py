907e0c9cd7a7d7a692bb85df6e6436c2b2519ba3
alexlim <aml924@nyu.edu>
2023-02-10 15:36:15 -0500

    need to figure out img upload

--- miniproject1/cocktail/recipe/cocktail_book/models.py
+++ miniproject1/cocktail/recipe/cocktail_book/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Vodka(models.Model):
     name = models.CharField(max_length=200)
-    ingredients = models.TextField(max_length='1000', default='')
-    instructions = models.TextField(max_length='1000', default='')
-    notes = models.TextField(max_length='1000', default='')
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    notes = models.TextField()
     image = models.ImageField(upload_to='img/', default='')
 
 class Rum(models.Model):
--- miniproject1/cocktail/recipe/recipe/settings.py
+++ miniproject1/cocktail/recipe/recipe/settings.py
@@ -158,5 +158,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 mimetypes.add_type("text/css", ".css", True)
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
